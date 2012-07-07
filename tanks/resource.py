9e1a8d5231b1b6edc43afe773de3fdbe5621e58d
Jonathan Hartley <tartley@tartley.com>
2012-07-07 13:15:52 +0100

    image loading now sets anchor point in middle of image

--- tanks/resource.py
+++ tanks/resource.py
@@ -4,17 +4,23 @@ import pyglet
 from pkg_resources import resource_listdir, resource_stream
 
 
-def load_image(filename):
-    return pyglet.image.load(
+def _get_name(filename):
+    return os.path.splitext(os.path.basename(filename))[0]
+
+
+def _load_image(filename):
+    texture = pyglet.image.load(
         filename,
         resource_stream('tanks', filename)
-    )
+    ).get_texture()
+    texture.anchor_x = texture.width / 2.0
+    texture.anchor_y = texture.height / 2.0
+    return texture
 
 
 def load_images(directory):
-    images = {}
-    for filename in resource_listdir('tanks', directory):
-        name, _ = os.path.splitext(os.path.basename(filename))
-        images[name] = load_image(directory + '/' + filename)
-    return images
+    return {
+        _get_name(filename): _load_image(directory + '/' + filename)
+        for filename in resource_listdir('tanks', directory)
+    }
 
