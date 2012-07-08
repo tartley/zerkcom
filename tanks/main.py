a2cf5a37d56d569a029d31529879cccb0d6f1302
Jonathan Hartley <tartley@tartley.com>
2012-07-08 17:59:42 +0100

    move pyglet.window creation and on_draw handler into view.window

--- tanks/main.py
+++ tanks/main.py
@@ -6,23 +6,17 @@ from . import keyhandlers
 from .model.world import World
 from .options import create_parser
 from .control.player import create_player
-from .resource import load_images
-from .view import render, window
+from .view import window
 
 
 # setup.py install/develop creates an executable that calls 'main()'
 def main():
     options = create_parser().parse_args(sys.argv[1:])
     world = World()
-    win = window.create(options)
-    draw_sprites = render.init(win, world, load_images('data/images'))
+    win = window.init(world, options)
+
     keyhandlers.init(win, world)
     world.add(create_player())
-
-    @win.event
-    def on_draw():
-        window.clear()
-        draw_sprites((item, item.position, item.angle) for item in world)
 
     def update(dt):
         for item in world:
--- tanks/view/window.py
+++ tanks/view/window.py
@@ -1,21 +1,34 @@
 import pyglet
 from pyglet import gl
 
+from ..resource import load_images
+from . import render
+
 
 CLEAR_COLOR_DEFAULT = (0.1, 0.2, 0.3, 1.0)
-
-
-def create(options):
-    return pyglet.window.Window(
-        fullscreen=options.fullscreen,
-        vsync=options.vsync,
-        resizable=not options.fullscreen,
-        caption='Tanks',
-    )
 
 
 def clear(color=CLEAR_COLOR_DEFAULT):
     r, g, b, _ = color
     gl.glClearColor(r, g, b, 1.0)
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+
+def init(world, options):
+
+    window = pyglet.window.Window(
+        fullscreen=options.fullscreen,
+        vsync=options.vsync,
+        resizable=not options.fullscreen,
+        caption='Tanks',
+    )
+
+    @window.event
+    def on_draw():
+        clear()
+        draw_sprites((item, item.position, item.angle) for item in world)
+
+    draw_sprites = render.init(window, world, load_images('data/images'))
+
+    return window
 
