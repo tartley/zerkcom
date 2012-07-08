be1834d19a1b2160a93803234eaaf2c58659e933
Jonathan Hartley <tartley@tartley.com>
2012-07-08 19:09:11 +0100

    remove our window.clear function, use rabbyt's instead

--- tanks/view/window.py
+++ tanks/view/window.py
@@ -8,10 +8,6 @@ from . import sprite
 CLEAR_COLOR_DEFAULT = (0.1, 0.3, 0.2)
 
 
-def clear(color=CLEAR_COLOR_DEFAULT):
-    rabbyt.clear(rgba=CLEAR_COLOR_DEFAULT)
-
-
 def init(world, options):
 
     window = pyglet.window.Window(
@@ -26,7 +22,7 @@ def init(world, options):
 
     @window.event
     def on_draw():
-        clear()
+        rabbyt.clear(rgba=CLEAR_COLOR_DEFAULT)
         draw_sprites((item, item.position, item.angle) for item in world)
 
     draw_sprites = sprite.init(window, world, load_all('data/images'))
