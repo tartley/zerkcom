a5ab284b733c6af02b721ff2e656d80be763e899
Jonathan Hartley <tartley@tartley.com>
2012-07-09 09:54:55 +0100

    modify rect to store a list of verts

--- tanks/model/rect.py
+++ tanks/model/rect.py
@@ -2,8 +2,14 @@
 class Rect(object):
 
     def __init__(self, left, bottom, right, top):
-        self.left = min(left, right)
-        self.right = max(left, right)
-        self.bottom = min(bottom, top)
-        self.top = max(bottom, top)
+        left = min(left, right)
+        right = max(left, right)
+        bottom = min(bottom, top)
+        top = max(bottom, top)
+        self.verts = [
+            (left, bottom),
+            (left, top),
+            (right, top),
+            (right, bottom),
+        ]
 
