62db76b9cc38f872a42a4f5ce776629fe0a6664b
Jonathan Hartley <tartley@tartley.com>
2012-07-09 09:54:35 +0100

    fix tank turning direction

--- tanks/control/tank.py
+++ tanks/control/tank.py
@@ -7,7 +7,7 @@ TURN_RATE = 0.02
 SPEED = 2.0
 
 def update_tank(item, dt):
-    item.angle += (item.speed_right - item.speed_left) * TURN_RATE
+    item.angle += (item.speed_left - item.speed_right) * TURN_RATE
     item.position += Vector(-sin(item.angle), cos(item.angle)) * \
         (item.speed_left + item.speed_right) * SPEED
 
